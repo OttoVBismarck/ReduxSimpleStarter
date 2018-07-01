8e64a68a68f3baec3d0733652b818757dd70dd00
ottovbismarck <fritzj96@gmail.com>
2018-07-01 05:12:38 -0700

    added my first-ever reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(params) {
+    return [
+        {title: 'book1'},
+        {title: 'Eloquent Ruby'},
+        {title: 'The Dark Tower'},
+        {title: 'lol the book'}
+    ]
+}
