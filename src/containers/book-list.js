9688175838744784541f0ecbef93d6bdbcb60c94
ottovbismarck <fritzj96@gmail.com>
2018-07-01 05:27:37 -0700

    Setting up container

---
+++ src/containers/book-list.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class BookList extends Component {
+    renderList() {
+        return this.props.books.map(book => {
+            return (
+                <li key={book.title} className="list-group-item">{book.title}</li>
+            )
+        })
+    }
+    render() {
+        return (
+            <ul className="col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    //whatever is returned from here will show up as props inside of booklist.
+}
